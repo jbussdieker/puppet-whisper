d66f884aa995afb50ff454931e86a7fa70f08672
Joshua B. Bussdieker <jbussdieker@gmail.com>
2014-08-18 15:04:01 -0700

    Install from source

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,21 @@
-class whisper($ensure = present) {
+class whisper(
+  $path = '/usr/local/src/whisper',
+  $source = 'https://github.com/graphite-project/whisper.git',
+  $revision = 'master'
+) {
 
-  package { 'python-whisper':
-    ensure => $ensure,
+  vcsrepo { $path:
+    ensure   => present,
+    revision => $revision,
+    source   => $source,
+    provider => git,
+  }
+
+  exec { 'install_whisper':
+    cwd     => $path,
+    command => "/usr/bin/python setup.py install",
+    creates => '/usr/local/lib/python2.7/dist-packages/whisper.py',
+    require => Vcsrepo[$path],
   }
 
 }
