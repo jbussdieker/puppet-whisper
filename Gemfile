5f7c2b115867e96f460632f69eef750865b81494
Joshua B. Bussdieker <jbussdieker@gmail.com>
2014-09-15 00:19:33 -0700

    Add beaker tests

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+
+gem 'puppet'
+gem 'rspec', '2.14.1'
+gem 'puppetlabs_spec_helper'
+gem 'beaker-rspec'
+gem 'pry'
---
+++ spec/acceptance/whisper_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper_acceptance'
+
+describe 'whisper class' do
+  let(:manifest) {
+    <<-EOS
+      class { 'whisper': }
+    EOS
+  }
+
+  describe 'running puppet code' do
+    it 'should work with no errors' do
+      # Run it twice and test for idempotency
+      apply_manifest(manifest, :catch_failures => true)
+      expect(apply_manifest(manifest, :catch_changes => true).exit_code).to be_zero
+    end
+
+    describe 'command line tools' do
+      before do
+        apply_manifest(manifest, :catch_failures => true)
+        apply_manifest(manifest, :catch_changes => true)
+      end
+
+      it 'whisper-create.py should work' do
+        shell("whisper-create.py --overwrite /tmp/testmetric.wsp 60:1440", :acceptable_exit_codes => 0)
+      end
+    end
+  end
+end
