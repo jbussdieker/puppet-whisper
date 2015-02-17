2ec0572e7c7c201dd58994b9471bcae091bba98c
Joshua B. Bussdieker <jbussdieker@brightroll.com>
2015-02-17 00:20:56 -0800

    Test based on facts

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,6 +1,76 @@
 require 'spec_helper'
 
 describe 'whisper' do
+  let(:facts) do
+    {
+      :operatingsystem => operatingsystem,
+      :operatingsystemmajrelease => operatingsystemmajrelease
+    }
+  end
+  let(:params) { { :ensure => ensure_value } }
+
+  let(:operatingsystem) { 'Ubuntu' }
+  let(:operatingsystemmajrelease) { '12.04' }
+
+  let(:ensure_value) { 'present' }
+
+  context 'CentOS' do
+    let(:operatingsystem) { 'CentOS' }
+
+    context '7' do
+      let(:operatingsystemmajrelease) { '7' }
+      it { should contain_class('whisper::source') }
+    end
+
+    context '6' do
+      let(:operatingsystemmajrelease) { '6' }
+      it { should contain_class('whisper::source') }
+    end
+
+    context '5' do
+      let(:operatingsystemmajrelease) { '5' }
+      it { should contain_class('whisper::source') }
+    end
+  end
+
+  context 'Debian' do
+    let(:operatingsystem) { 'Debian' }
+
+    context '7' do
+      let(:operatingsystemmajrelease) { '7' }
+      it { should contain_class('whisper::package') }
+    end
+
+    context '6' do
+      let(:operatingsystemmajrelease) { '6' }
+      it { should contain_class('whisper::package') }
+    end
+  end
+
+  context 'Ubuntu' do
+    let(:operatingsystem) { 'Ubuntu' }
+
+    context '14.04' do
+      let(:operatingsystemmajrelease) { '14.04' }
+      it { should contain_class('whisper::package') }
+    end
+
+    context '13.10' do
+      let(:operatingsystemmajrelease) { '13.10' }
+      it { should contain_class('whisper::package') }
+    end
+
+    context '12.04' do
+      let(:operatingsystemmajrelease) { '12.04' }
+      it { should contain_class('whisper::package') }
+    end
+
+    context '10.04' do
+      let(:operatingsystemmajrelease) { '10.04' }
+      it { should contain_class('whisper::source') }
+    end
+  end
+
   context 'default params' do
     it { should have_resource_count(1) }
 
@@ -11,7 +81,6 @@ describe 'whisper' do
 
   context 'custom params' do
     let(:ensure_value) { '0.0.1' }
-    let(:params) { { :ensure => ensure_value } }
 
     it { should have_resource_count(2) }
 
