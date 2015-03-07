692dce51acb612a7091b2806cb0e45d3630c3f11
Joshua B. Bussdieker <jbussdieker@brightroll.com>
2015-03-06 20:45:15 -0800

    More acceptance testing

--- spec/acceptance/whisper_package_spec.rb
+++ spec/acceptance/whisper_package_spec.rb
@@ -1,27 +1,35 @@
 require 'spec_helper_acceptance'
 
-describe 'whisper class' do
+describe 'whisper::package class' do
   let(:manifest) {
     <<-EOS
-      class { 'whisper': }
+      class { 'whisper::package':
+
+      }
     EOS
   }
 
   describe 'running puppet code' do
     it 'should work with no errors' do
-      # Run it twice and test for idempotency
       apply_manifest(manifest, :catch_failures => true)
       expect(apply_manifest(manifest, :catch_changes => true).exit_code).to be_zero
     end
-
   end
 
   describe 'command line tools' do
     it 'whisper-create.py should work' do
-      if shell("which whisper-create.py").exit_code == 0
-        shell("whisper-create.py --overwrite /tmp/testmetric.wsp 60:1440", :acceptable_exit_codes => 0)
-      else
+      if shell("which whisper-create").exit_code == 0
         shell("whisper-create --overwrite /tmp/testmetric.wsp 60:1440", :acceptable_exit_codes => 0)
+      else
+        shell("whisper-create.py --overwrite /tmp/testmetric.wsp 60:1440", :acceptable_exit_codes => 0)
+      end
+    end
+
+    it 'whisper-info.py should work' do
+      if shell("which whisper-info").exit_code == 0
+        shell("whisper-info /tmp/testmetric.wsp", :acceptable_exit_codes => 0)
+      else
+        shell("whisper-info.py /tmp/testmetric.wsp", :acceptable_exit_codes => 0)
       end
     end
   end
--- spec/acceptance/whisper_source_spec.rb
+++ spec/acceptance/whisper_source_spec.rb
@@ -15,15 +15,15 @@ describe 'whisper::source class' do
       apply_manifest(manifest, :catch_failures => true)
       expect(apply_manifest(manifest, :catch_changes => true).exit_code).to be_zero
     end
+  end
 
-    describe 'command line tools' do
-      it 'whisper-create.py should work' do
-        shell("whisper-create.py --overwrite /tmp/testmetric.wsp 60:1440", :acceptable_exit_codes => 0)
-      end
+  describe 'command line tools' do
+    it 'whisper-create.py should work' do
+      shell("whisper-create.py --overwrite /tmp/testmetric.wsp 60:1440", :acceptable_exit_codes => 0)
+    end
 
-      it 'whisper-info.py should work' do
-        shell("whisper-info.py /tmp/testmetric.wsp", :acceptable_exit_codes => 0)
-      end
+    it 'whisper-info.py should work' do
+      shell("whisper-info.py /tmp/testmetric.wsp", :acceptable_exit_codes => 0)
     end
   end
 end
